bd44ce593fa85c50b2a7ea3c7899cede798fbf79
botmenoff <marvenferry60@gmail.com>
2024-08-05 00:27:50 +0200

    Added to updated route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,7 +10,7 @@ const UserMiddlewares = require('../middlewares/user.middlewares')
 router.post('/user/register', UserMiddlewares.registerMiddleware, UserController.register);
 router.get('/user/verify/:jwt', UserController.verifyEmail);
 router.get('/user/login', UserController.login);
-router.put('/user/update/:id', UserController.updateUser)
+router.put('/user/update/:id', UserMiddlewares.verifyOwner, UserController.updateUser)
 router.delete('/user/delete/:id', UserMiddlewares.verifyOwner, UserController.deleteUsersById);
 
 
